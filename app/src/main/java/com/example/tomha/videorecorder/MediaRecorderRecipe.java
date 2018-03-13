1de86d1f624dd72bdcacd615b59877d203afd377
TomHartogs <tom.hartogs@greenhousegroup.com>
2018-03-13 19:40:16 +0100

    First push simple video recorder

---
+++ app/src/main/java/com/example/tomha/videorecorder/MediaRecorderRecipe.java
@@ -0,0 +1,247 @@
+package com.example.tomha.videorecorder;
+
+/**
+ * Created by tomha on 27-2-2018.
+ */
+
+import java.io.IOException;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.hardware.Camera;
+import android.media.CamcorderProfile;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.File;
+import java.lang.reflect.Parameter;
+import java.util.List;
+
+public class MediaRecorderRecipe extends Activity implements SurfaceHolder.Callback {
+
+    private MediaRecorder mMediaRecorder;
+    private Camera mCamera;
+    private SurfaceView mSurfaceView;
+    private SurfaceHolder mHolder;
+    private Button mButton;
+    private boolean mInitSuccesful;
+    private File mOutputFile;
+    private boolean recording = false;
+    private CountDownTimer timer;
+    //private TextView resterendeText;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.media_recorder_recipe);
+
+        //resterendeText = findViewById(R.id.resterendTextView);
+
+        mButton = findViewById(R.id.recordingButton);
+        mButton.setOnClickListener(new OnClickListener() {
+            @Override
+            // toggle video recording
+            public void onClick(View v) {
+                if(!recording){
+                    try {
+                        initRecorder(mHolder.getSurface());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    mCamera.unlock();
+                    mMediaRecorder.start();
+                    recording = true;
+                    mButton.setVisibility(View.INVISIBLE);
+                    Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            mButton.setVisibility(View.VISIBLE);
+                            mButton.setText("Stop");
+                            //mButton.setBackgroundColor(Color.RED);
+                            mButton.getBackground().setColorFilter(Color.RED, PorterDuff.Mode.MULTIPLY);
+                        }
+
+                    }, 500); // 5000ms delay
+                    //timer.start();
+                }
+                else{
+                    mMediaRecorder.stop();
+                    mMediaRecorder.reset();
+                    recording = false;
+                    //timer.cancel();
+                    //resterendeText.setVisibility(View.INVISIBLE);
+                    mButton.setText("Start");
+                    mButton.getBackground().setColorFilter(Color.WHITE, PorterDuff.Mode.MULTIPLY);
+                }
+            }
+        });
+
+        /*timer = new CountDownTimer((getIntent().getIntExtra("delay", 10)*1000), 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                resterendeText.setText("Seconden resterend: " + millisUntilFinished / 1000);
+                resterendeText.setVisibility(View.VISIBLE);
+            }
+
+            public void onFinish() {
+                resterendeText.setVisibility(View.INVISIBLE);
+            }
+        };*/
+
+        // we shall take the video in landscape orientation
+
+        mSurfaceView = findViewById(R.id.surfaceView);
+        mHolder = mSurfaceView.getHolder();
+        mHolder.addCallback(this);
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+    /* Init the MediaRecorder, the order the methods are called is vital to
+     * its correct functioning */
+    private void initRecorder(Surface surface) throws IOException {
+        // It is very important to unlock the camera before doing setCamera
+        // or it will results in a black preview
+
+        if (mMediaRecorder == null) mMediaRecorder = new MediaRecorder();
+        mMediaRecorder.setPreviewDisplay(surface);
+        if(mCamera == null){
+            initCamera();
+        }
+        mMediaRecorder.setCamera(mCamera);
+        VideoCapture t = new VideoCapture();
+        t.startVideoRecording(getIntent().getIntExtra("delay", 10)); //seconds
+        mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
+        mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+        //mMediaRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH));
+        mMediaRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_720P));
+        mOutputFile = CameraHelper.getOutputMediaFile(CameraHelper.MEDIA_TYPE_VIDEO, "StudioSurprise");
+
+        if (mOutputFile != null) {
+            mMediaRecorder.setOutputFile(mOutputFile.getPath());
+        }
+        else{
+            mMediaRecorder = null;
+        }
+
+        try {
+            mMediaRecorder.prepare();
+        } catch (IllegalStateException e) {
+            // This is thrown if the previous calls are not called with the
+            // proper order
+            e.printStackTrace();
+        }
+        mInitSuccesful = true;
+    }
+
+    private void initCamera(){
+        List<Camera.Size> mSupportedPreviewSizes;
+        Camera.Size mPreviewSize;
+        mCamera = CameraHelper.getDefaultFrontFacingCameraInstance();
+        mSupportedPreviewSizes = mCamera.getParameters().getSupportedPreviewSizes();
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int height = displayMetrics.heightPixels;
+        int width = displayMetrics.widthPixels;
+        mPreviewSize = getOptimalPreviewSize(mSupportedPreviewSizes, width, height);
+        Camera.Parameters parameters = mCamera.getParameters();
+        parameters.setPreviewSize(mPreviewSize.width, mPreviewSize.height);
+        mCamera.setParameters(parameters);
+    }
+
+    private Camera.Size getOptimalPreviewSize(List<Camera.Size> sizes, int w, int h) {
+        final double ASPECT_TOLERANCE = 0.1;
+        double targetRatio=(double)h / w;
+
+        if (sizes == null) return null;
+
+        Camera.Size optimalSize = null;
+        double minDiff = Double.MAX_VALUE;
+
+        int targetHeight = h;
+
+        for (Camera.Size size : sizes) {
+            double ratio = (double) size.width / size.height;
+            if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) continue;
+            if (Math.abs(size.height - targetHeight) < minDiff) {
+                optimalSize = size;
+                minDiff = Math.abs(size.height - targetHeight);
+            }
+        }
+
+        if (optimalSize == null) {
+            minDiff = Double.MAX_VALUE;
+            for (Camera.Size size : sizes) {
+                if (Math.abs(size.height - targetHeight) < minDiff) {
+                    optimalSize = size;
+                    minDiff = Math.abs(size.height - targetHeight);
+                }
+            }
+        }
+        return optimalSize;
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        initCamera();
+        try {
+            mCamera.setPreviewDisplay(holder);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mCamera.startPreview();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        shutdown();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+    }
+
+    private void shutdown() {
+        // Release MediaRecorder and especially the Camera as it's a shared
+        // object that can be used by other applications
+        if(mMediaRecorder != null) {
+            mMediaRecorder.reset();
+            mMediaRecorder.release();
+            mMediaRecorder = null;
+        }
+        if(mCamera != null) {
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
+    public class VideoCapture extends Activity implements MediaRecorder.OnInfoListener {
+
+        public void startVideoRecording(int delay) {
+            // Normal MediaRecorder Setup
+            mMediaRecorder.setMaxDuration(delay * 1000);
+            mMediaRecorder.setOnInfoListener(this);
+        }
+
+        public void onInfo(MediaRecorder mr, int what, int extra) {
+            if (what == MediaRecorder.MEDIA_RECORDER_INFO_MAX_DURATION_REACHED) {
+                Log.v("VIDEOCAPTURE","Maximum Duration Reached");
+                mButton.callOnClick();
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/tomha/videorecorder/RecordActivity.java
@@ -0,0 +1,189 @@
+package com.example.tomha.videorecorder;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.content.res.Configuration;
+import android.graphics.PixelFormat;
+import android.os.IBinder;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.text.Editable;
+import android.text.InputType;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class RecordActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_REQUEST_CAMERA = 0;
+    private static final int PERMISSION_REQUEST_STORAGE = 1;
+    private static final int PERMISSION_REQUEST_RECORD_AUDIO = 2;
+
+    private boolean storagePermission = false;
+    private boolean cameraPermission = false;
+    private boolean recordAudioPermission = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+
+        checkPermissions();
+
+        /*final AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Maximale opname duur");
+        alert.setMessage("Geef aan hoe lang de opname maximaal mag duren (in seconden).");
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_NUMBER);
+        input.setRawInputType(Configuration.KEYBOARD_12KEY);
+        alert.setView(input);
+
+        final Intent intent = new Intent(this, MediaRecorderRecipe.class);
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                int delay = Integer.parseInt(input.getText().toString());
+
+                // Do something with value!
+                //startRecordingActivity();
+                intent.putExtra("delay", delay);
+                //alert = new AlertDialog.Builder(getApplicationContext());
+                alert.setTitle("Evenementnaam");
+                alert.setMessage("Voer naam evenement in.");
+                final EditText inputName = new EditText(getApplicationContext());
+                alert.setView(inputName);
+
+                alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        String prename = inputName.getText().toString();
+
+                        // Do something with value!
+                        //startRecordingActivity();
+                        intent.putExtra("prename", prename);
+                        startActivity(intent);
+                    }
+                });
+                alert.show();
+            }
+        });
+        alert.show();*/
+        final Intent intent = new Intent(this, MediaRecorderRecipe.class);
+        intent.putExtra("delay", 60*5);
+        startActivity(intent);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_CAMERA) {
+            // Request for camera permission.
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission has been granted. Start camera preview Activity.
+                cameraPermission = true;
+            }
+        }
+        if (requestCode == PERMISSION_REQUEST_STORAGE) {
+            // Request for camera permission.
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission has been granted. Start camera preview Activity.
+                storagePermission = true;
+            }
+        }
+        if (requestCode == PERMISSION_REQUEST_RECORD_AUDIO) {
+            // Request for camera permission.
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission has been granted. Start camera preview Activity.
+                recordAudioPermission = true;
+            }
+        }
+        if(cameraPermission && storagePermission && recordAudioPermission) {
+            startRecordingActivity();
+        }
+        else{
+            checkPermissions();
+        }
+    }
+
+    private void showRecordingActivity() {
+        // Check if the Camera permission has been granted
+        if (cameraPermission && storagePermission && recordAudioPermission) {
+            startRecordingActivity();
+        } else {
+            checkPermissions();
+            //Loop till permissions granted
+            showRecordingActivity();
+        }
+    }
+
+    private void checkPermissions(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            cameraPermission = true;
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
+            recordAudioPermission = true;
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, PERMISSION_REQUEST_CAMERA);
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            storagePermission = true;
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_STORAGE);
+        }
+    }
+
+    private void startRecordingActivity() {
+        Intent intent = new Intent(this, MediaRecorderRecipe.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // nothing to do here
+        // … really
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if(!hasFocus) {
+            // Close every kind of system dialog
+            Intent closeDialog = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
+            sendBroadcast(closeDialog);
+        }
+    }
+
+    private final List blockedKeys = new ArrayList(Arrays.asList(KeyEvent.KEYCODE_VOLUME_DOWN, KeyEvent.KEYCODE_VOLUME_UP));
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        if (blockedKeys.contains(event.getKeyCode())) {
+            return true;
+        } else {
+            return super.dispatchKeyEvent(event);
+        }
+    }
+}
+
