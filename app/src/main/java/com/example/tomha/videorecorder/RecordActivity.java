46580ed17be1ccb18c81c7cb12b0e25ff8523b64
TomHartogs <tomhartogs@gmail.com>
2018-03-18 15:54:25 +0100

    Simplify restriction checking

--- app/src/main/java/com/example/tomha/videorecorder/RecordActivity.java
+++ app/src/main/java/com/example/tomha/videorecorder/RecordActivity.java
@@ -1,34 +1,15 @@
 package com.example.tomha.videorecorder;
 
 import android.Manifest;
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.ComponentName;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
-import android.content.res.Configuration;
-import android.graphics.PixelFormat;
-import android.os.IBinder;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
-import android.text.Editable;
-import android.text.InputType;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.KeyEvent;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.ToggleButton;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -36,151 +17,53 @@ import java.util.List;
 
 public class RecordActivity extends AppCompatActivity {
 
-    private static final int PERMISSION_REQUEST_CAMERA = 0;
-    private static final int PERMISSION_REQUEST_STORAGE = 1;
-    private static final int PERMISSION_REQUEST_RECORD_AUDIO = 2;
-
-    private boolean storagePermission = false;
-    private boolean cameraPermission = false;
-    private boolean recordAudioPermission = false;
+    private static final String[] PERMISSIONS = {Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    private static final int PERMISSION_ALL = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
 
-        checkPermissions();
+        if(!hasPermissions(this, PERMISSIONS)){
+            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
+        } else {
+            startRecordingActivity();
+        }
+    }
 
-        /*final AlertDialog.Builder alert = new AlertDialog.Builder(this);
-        alert.setTitle("Maximale opname duur");
-        alert.setMessage("Geef aan hoe lang de opname maximaal mag duren (in seconden).");
-        final EditText input = new EditText(this);
-        input.setInputType(InputType.TYPE_CLASS_NUMBER);
-        input.setRawInputType(Configuration.KEYBOARD_12KEY);
-        alert.setView(input);
-
-        final Intent intent = new Intent(this, MediaRecorderRecipe.class);
-        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int whichButton) {
-                int delay = Integer.parseInt(input.getText().toString());
-
-                // Do something with value!
-                //startRecordingActivity();
-                intent.putExtra("delay", delay);
-                //alert = new AlertDialog.Builder(getApplicationContext());
-                alert.setTitle("Evenementnaam");
-                alert.setMessage("Voer naam evenement in.");
-                final EditText inputName = new EditText(getApplicationContext());
-                alert.setView(inputName);
-
-                alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        String prename = inputName.getText().toString();
-
-                        // Do something with value!
-                        //startRecordingActivity();
-                        intent.putExtra("prename", prename);
-                        startActivity(intent);
-                    }
-                });
-                alert.show();
+    public static boolean hasPermissions(Context context, String[] permissions) {
+        if (context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_DENIED) {
+                    return false;
+                }
             }
-        });
-        alert.show();*/
-        showRecordingActivity();
+        }
+        return true;
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions,
                                            int[] grantResults) {
-        if (requestCode == PERMISSION_REQUEST_CAMERA) {
-            // Request for camera permission.
-            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permission has been granted. Start camera preview Activity.
-                cameraPermission = true;
+        if(grantResults.length > 0) {
+            ArrayList<String> deniedPermissions = new ArrayList<>();
+            for (int i = 0; i < grantResults.length; i++){
+                if(grantResults[i] == PackageManager.PERMISSION_DENIED){
+                    deniedPermissions.add(permissions[i]);
+                }
             }
-        }
-        if (requestCode == PERMISSION_REQUEST_STORAGE) {
-            // Request for camera permission.
-            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permission has been granted. Start camera preview Activity.
-                storagePermission = true;
+            if(!deniedPermissions.isEmpty()){
+                ActivityCompat.requestPermissions(this, deniedPermissions.toArray(new String[0]), requestCode);
+            } else if (hasPermissions(this, PERMISSIONS)){
+                startRecordingActivity();
             }
-        }
-        if (requestCode == PERMISSION_REQUEST_RECORD_AUDIO) {
-            // Request for camera permission.
-            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permission has been granted. Start camera preview Activity.
-                recordAudioPermission = true;
-            }
-        }
-        if(cameraPermission && storagePermission && recordAudioPermission) {
-            startRecordingActivity();
-        }
-        else{
-            checkPermissions();
-        }
-    }
-
-    private void showRecordingActivity() {
-        // Check if the Camera permission has been granted
-        if (cameraPermission && storagePermission && recordAudioPermission) {
-            startRecordingActivity();
-        } else {
-            checkPermissions();
-            //Loop till permissions granted
-            showRecordingActivity();
-        }
-    }
-
-    private void checkPermissions(){
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
-            cameraPermission = true;
-        } else {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
-        }
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
-            recordAudioPermission = true;
-        } else {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, PERMISSION_REQUEST_CAMERA);
-        }
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-            storagePermission = true;
-        } else {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_STORAGE);
         }
     }
 
     private void startRecordingActivity() {
         Intent intent = new Intent(this, MediaRecorderRecipe.class);
         startActivity(intent);
-    }
-
-    @Override
-    public void onBackPressed() {
-        // nothing to do here
-        // … really
-    }
-
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        if(!hasFocus) {
-            // Close every kind of system dialog
-            Intent closeDialog = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
-            sendBroadcast(closeDialog);
-        }
-    }
-
-    private final List blockedKeys = new ArrayList(Arrays.asList(KeyEvent.KEYCODE_VOLUME_DOWN, KeyEvent.KEYCODE_VOLUME_UP));
-
-    @Override
-    public boolean dispatchKeyEvent(KeyEvent event) {
-        if (blockedKeys.contains(event.getKeyCode())) {
-            return true;
-        } else {
-            return super.dispatchKeyEvent(event);
-        }
     }
 }
 
